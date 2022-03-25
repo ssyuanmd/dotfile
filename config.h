2f550224d52bdfb916eeb7bd277a5dc4e63c2c4e
wu syuand <100557746+ssyuanmd@users.noreply.github.com>
2022-03-26 00:48:42 +0800

    Update config.h

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static const char *roficmd[] = { "rofi", "-show", "drun", "-show-icons", NULL };
 static const unsigned int borderpx  = 7;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
